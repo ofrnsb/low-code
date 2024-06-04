9a05f84eb89e151df708fadbc6bdda0a9d2aab1e
ofrnsb <ondifransbutarbutar@upi.edu>
2024-06-04 11:00:11 +0700

    delete unused codes

--- backup, ga make git soalnya wkwkwk/Data/CONST.js
+++
@@ -1,3 +0,0 @@
-export const cssUnitRegex = {
-  CSSunit: /^(?:-?\d+\.?\d*|\.\d+)(px|em|rem|%|vh|vw|cm|mm|in|pt|pc|ex|ch)$/i,
-};
--- backup, ga make git soalnya wkwkwk/Utils/colorFormatter.js
+++
@@ -1,9 +0,0 @@
-export function rgbToHex(rgb) {
-  return (
-    '#' +
-    rgb
-      .match(/\d+/g)
-      .map((x) => (+x).toString(16).padStart(2, '0'))
-      .join('')
-  );
-}
